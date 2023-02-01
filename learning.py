21aa9d096f1802013b5400567bebeb88a85c5f68
rsimai <robert.simai@.suse.com>
2023-02-01 17:28:56 +0100

    up

--- learning.py
+++ learning.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import tensorflow as tf
 from tensorflow import keras
@@ -7,14 +7,15 @@ model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-xs=[1, 3, 5]
-ys=[2, 6, 10]
+xs=[0, 2, 4, 6, 8, 10]         # input
+ys=[0, 4, 8, 12, 16, 20]       # expected output
 
-model.fit(xs, ys, epochs=1000)
+model.fit(xs, ys, epochs=1000) # training
 
 while(True):
     number = input("Number: ")
     n = float(number)
-    print("Result:", model.predict([n]))
+    o = float(model.predict([n]))
+    print("Result:", o)
 
 
