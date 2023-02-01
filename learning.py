5f7d720e86148f5bbc51562ff75f98bd184682c1
rsimai <robert.simai@.suse.com>
2023-02-01 16:59:08 +0100

    initial

---
+++ learning.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import tensorflow as tf
+from tensorflow import keras
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+xs=[1, 3, 5]
+ys=[2, 6, 10]
+
+model.fit(xs, ys, epochs=1000)
+
+print(model.predict([7]))
+
+
